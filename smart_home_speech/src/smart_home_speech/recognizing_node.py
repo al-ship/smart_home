b138ea061b7c9b607799e2c7d0cbddafbb7c857b
chaosal <chaosal@yandex.ru>
2013-07-24 15:39:07 +0600

    распознавание через google api

--- smart_home_speech/src/smart_home_speech/recognizing_node.py
+++ smart_home_speech/src/smart_home_speech/recognizing_node.py
@@ -3,7 +3,8 @@
 
 import rospi
 import json
-import codecs
+#import codecs
+import shlex,subprocess
 from std_msgs.msg import String
 from smart_home_core.srv import *
 from smart_home_core.msg import Notification
@@ -49,9 +50,25 @@ class recognizer(object):
 	        command = rospy.ServiceProxy('command', Command)
 		resp1 = command(st)		    
 	    except rospy.ServiceException, e:
-	        print "Service call failed: %s"%e	
+	        print "Service call failed: %s"%e
+    
+    def recognizing(self):
+        rec_args = shlex.split('sox -r 16000 -t alsa default recording.flac silence 1 0.1 1% 1 1.5 1%')
+	google_args = shlex.split('wget -q -U "Mozilla/5.0" --post-file recording.flac --header="Content-Type: audio/x-flac; rate=16000" -O - "http://www.google.com/speech-api/v1/recognize?lang=ru-ru&client=chromium"')
+	# rec sound
+	try:
+	    subprocess.call(rec_args)
+	except:
+	    rospy.logerr('error recording sound')
+	# recognizing
+	output,error = subprocess.Popen(args2,stdout = subprocess.PIPE, stderr= subprocess.PIPE).communicate()
+        if not error and len(output)>16:
+	    a = eval(output)
+	    return String(a['hypotheses'][0]['utterance'])	    	    
 
 if __name__=="__main__":
     r = recognizer()
     while not rospy.is_shutdown():
-        r.spin()   
+        text = r.recognizing()
+	if text:
+	    r.on_speech(text)
