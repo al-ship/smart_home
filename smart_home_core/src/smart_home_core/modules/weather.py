389194a502560c2abfb3b0126a24a35f97a90cb3
chaosal <chaosal@yandex.ru>
2013-07-29 16:23:07 +0600

    meteoinfo as weather service

--- smart_home_core/src/smart_home_core/modules/weather.py
+++ smart_home_core/src/smart_home_core/modules/weather.py
@@ -10,14 +10,15 @@ import sys
 
 class weather(basemodule.basemodule):
     def exec_cmd(self, params):
-        args = shlex.split('wget -q -O - "http://informer.gismeteo.ru/rss/28440.xml"')
+        #args = shlex.split('wget -q -O - "http://informer.gismeteo.ru/rss/28440.xml"')
+        args = shlex.split('wget -q -O - "http://meteoinfo.ru/rss/forecasts/28440"')
         out,err = subprocess.Popen(args, stdout = subprocess.PIPE, stderr= subprocess.PIPE).communicate()
         if not err and len(out) > 20:
             result = ''
             root = ET.fromstring(out)
             txt = 'погода в екатеринбурге '
             if 'tomorrow' in params:
-                txt = txt + 'на завтра. ' + root.find("./channel/item[last()]/description").text.encode('utf-8')
+                txt = txt + 'на завтра. ' + root.find("./channel/item[last()-1]/description").text.encode('utf-8')
             else:
                 txt = txt + '.' + root.find("./channel/item/description").text.encode('utf-8')
             return txt
