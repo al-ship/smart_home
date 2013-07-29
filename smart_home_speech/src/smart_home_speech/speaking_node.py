fd3d19857f2452c5d9576bead5d37533ea117e15
chaosal <chaosal@yandex.ru>
2013-07-29 17:05:50 +0600

    done. Not tested

--- smart_home_speech/src/smart_home_speech/speaking_node.py
+++ smart_home_speech/src/smart_home_speech/speaking_node.py
@@ -8,22 +8,32 @@ import roslib; roslib.load_manifest('sound_play')
 from sound_play.libsoundplay import SoundClient
 from std_msgs.msg import String
 
-#throttle = 3 # seconds
+class Speaking(object)
+    
+    def say(data):
+        text = data.data
+        for key in self.word_map.keys(): # replacing 'bad' words
+	    text = text.replace(key, self.word_map[key])
+        soundhandle.say(text, 'Elena')
+        rospy.sleep(1)
 
-def say(data):
-    soundhandle = SoundClient()
-    soundhandle.say(data.data, 'Elena')
-    rospy.sleep(1)
+    def __init__():
+        rospy.init_node('speaking_node', anonymous = True)
+    
+        self.wordmap = {} # dict. for replacing 'bad' words
+        dic_file_path  = rospy.get_param('~mapfile','russian.map')
+        try:
+            dic_file = codecs.open(dic_file_path, 'r', 'utf-8')    
+            self.word_map = json.load(dic_file)
+	    dic_file.close()
+        except:
+            rospy.logerr('Can not open mapping file %s' % dic_file_path)
 
-def speaking_node():
-    rospy.init_node('speaking_node', anonymous = True)
-    #global allow_yak
-    #allow_yak = rospy.Time.now()
-    rospy.Subscriber('speak', String, say)
-    rospy.spin()
+        rospy.Subscriber('speak', String, say)
+
 
 if __name__ == '__main__':
-    #soundhandle = SoundClient()
+    speaking = Speaking()
+    speaking.soundhandle = SoundClient()
     rospy.sleep(1)
-       
-    speaking_node()
+    rospy.spin()
