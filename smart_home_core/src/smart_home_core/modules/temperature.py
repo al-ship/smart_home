9fea27c1083dbbb041135a51d658d3f79d94c910
chaosal <chaosal@yandex.ru>
2013-08-09 18:03:34 +0600

    add module temperature

---
+++ smart_home_core/src/smart_home_core/modules/temperature.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys
+import basemodule
+
+class Temperature(basemodule.basemodule):
+    
+    path = '/mnt/1wire/28.87B236040000/fasttemp'
+    
+    def exec_cmd(self, params):
+        f = open(self.path, 'r')
+        data = f.read()
+        f.close()
+	data = data.strip().replace('-', 'минус ')
+	if '.' in data:
+	    data = data.replace('.', ' и ') + ' десятых градуса'
+	else:
+	    data = data + ' градусов'
+        return 'температура %s' % data
+        
+if __name__ == "__main__":
+    t = Temperature();
+    print t.exec_cmd(sys.argv)
