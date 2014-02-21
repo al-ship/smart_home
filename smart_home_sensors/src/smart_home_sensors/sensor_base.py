3d0eef0680810f72f55ccb5bc715983c92e43aab
chaosal <chaosal@yandex.ru>
2014-02-21 17:25:59 +0600

    te

--- smart_home_sensors/src/smart_home_sensors/sensor_base.py
+++ smart_home_sensors/src/smart_home_sensors/sensor_base.py
@@ -1,11 +1,101 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #Base class for sensors
+import rospy
+from std_msgs.msg import String
+from smart_home_sensors.srv import *
 
+"""
+Base class for sensor nodes
+"""
 class SensorBase(object):
 
+    """
+    Reads sensor data (low level).
+    Returns object of type get_message_class
+    Override this in child class
+    """
     def read(self):
-        pass
+       return self.get_message_class()()
 
+    """
+    writes sensor data (low level).
+    Override this in child class
+    """
     def write(self, data):
         pass
+
+    """
+    returns class of publishing message.
+    Override this in child class
+    """
+    def get_message_class(self):
+        return String
+
+    """
+    returns service class for reading sensor data.
+    Override this
+    """
+    def get_read_srv_class(self):
+        return None
+
+    """
+    Sensor reading handler.
+    Returns srv class.
+    Override this
+    """
+    def read_srv_handle(self, req):
+        return self.get_read_srv_class()()
+
+    """
+    returns service class for writing data to sensor.
+    Override this
+    """
+    def get_write_srv_class(self):
+        return None
+
+    """
+    Sensor writing handler
+    Override this
+    """
+    def write_srv_handle(self, req):
+        return self.get_write_srv_class()
+
+    """
+    name of node by default
+    """
+    def get_name(self):
+        return "sensor_base"
+
+
+    def __init__(self):
+        # init publisher for current sensor and message
+        self.timer = None
+        rospy.init_node(self.get_name())
+        if self.get_message_class is not None:
+            self.pub = rospy.Publisher(rospy.get_name(), self.get_message_class())
+        self.interval = rospy.get_param('~check_interval', 10)
+        # init service for reading and writing
+        if self.get_read_srv_class() is not None:
+            reader = rospy.Service(rospy.get_name() + '/read', self.get_read_srv_class(), self.read_srv_handle)
+        if self.get_write_srv_class() is not None:
+            writer = rospy.Service(rospy.get_name() + '/write', self.get_write_srv_class(), self.write_srv_handle)
+        if self.pub:
+            self.timer = rospy.Timer(rospy.Duration(self.interval), self.timer_call)
+
+    def timer_call(self, event):
+        try:
+            self.pub.publish(self.read())
+        except:
+            rospy.logerr('sensor read error %s' % rospy.get_name())
+
+    def stop(self):
+        if self.timer is not None:
+            self.timer.shutdown()
+
+if __name__ == "__main__":
+    sensor = SensorBase()
+    rospy.spin()
+    sensor.stop()
+
+
--- smart_home_sensors/src/smart_home_sensors/sensor_notifier_node.py
+++ smart_home_sensors/src/smart_home_sensors/sensor_notifier_node.py
@@ -5,9 +5,11 @@
 
 import rospy
 import uuid
-from sensor_base import SensorBase
-from fs_sensor import FsSensor
+#from sensor_base import SensorBase
+#from fs_sensor import FsSensor
 from smart_home_core.msg import Notification
+import rostopic
+import roslib
 
 class Notifier(object):
 
@@ -15,41 +17,35 @@ class Notifier(object):
         self.pub = rospy.Publisher('notification', Notification)
         rospy.init_node('sensor_notifier_node')
 
-        sensor_type = rospy.get_param('~sensor_type','fs')
-        sensor_param = rospy.get_param('~sensor_param', '')
-        self.init_sensor(sensor_type, sensor_param)
+        sensor_topic = rospy.get_param('~sensor_topic', None)
+        if sensor_topic == None:
+            rospy.logerr('param sensor_topic not defined')
 
-        self.notification_text = rospy.get_param('~notification_text', u'Сработал датчик')
+        self.notification_text = rospy.get_param('~notification_text', u'sensor activated')
         self.notification_level = rospy.get_param('~notification_level', 2)
         self.notification_target = rospy.get_param('~notification_target', 'voice,log')
-        self.notification_value = str(rospy.get_param('~notification_value', '1'))
-
-        interval = rospy.get_param('~check_inteval', 10)
-        self.timer = rospy.Timer(rospy.Duration(interval), self.timer_call)
-
-    def init_sensor(self, sensor_type, param):
-        if sensor_type == 'fs':
-            self.sensor = FsSensor(param)
+        self.notification_value = rospy.get_param('~notification_value', '1')
+        self.criteria = rospy.get_param('~criteria', 'eq')
+        # subscribe to sensor topic
+        data_type = rostopic.get_topic_type(sensor_topic, blocking=False)[0]
+        if data_type:
+            data_class = roslib.message.get_message_class(data_type)
+            rospy.Subscriber(sensor_topic, data_class, self.callback)
+            rospy.loginfo("start listening to %s, notification: %s %s", sensor_topic, self.criteria, str(self.notification_value))
         else:
-            rospy.logerr('sensor type %s not found' % sensor_type)
+            rospy.logerr("error getting type for topic " + sensor_topic)
 
-    def stop(self):
-        self.timer.shutdown()
-
-    def timer_call(self, event):
-        data = ''
-        try:
-            data = self.sensor.read().strip()
-        except:
-            rospy.logerr('Ошибка чтения датчика %s' % rospy.get_name())
-        #print data + self.notification_value
-        if self.notification_value == str(data):
-            #print self.notification_text
+    def callback(self, value):
+        #rospy.loginfo(str(value.data) + self.criteria + str(self.notification_value)
+        # value mast be one of std_msgs (field data)
+        if (self.criteria == 'eq' and value.data == self.notification_value ) or \
+           (self.criteria == 'gt' and value.data > self.notification_value ) or \
+           (self.criteria == 'lt' and value.data < self.notification_value ) or \
+           (self.criteria == 'ge' and value.data >= self.notification_value ) or \
+           (self.criteria == 'le' and value.data <= self.notification_value ) or \
+           (self.criteria == 'ne' and value.data != self.notification_value ):
             self.pub.publish(Notification(str(uuid.uuid1()), self.notification_text, self.notification_level, self.notification_target, "", ()))
-#	else:
-        #print self.notification_value + "!=" + data
 
 if __name__ == "__main__":
     notifier = Notifier()
     rospy.spin()
-    notifier.stop()
