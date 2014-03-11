b392885dbe71f574a9f4f8cc7e3ad57e1db9a251
chaosal <chaosal@yandex.ru>
2014-03-11 16:31:06 +0600

    fix sensors notification

--- smart_home_sensors/src/smart_home_sensors/sensor_notifier_node.py
+++ smart_home_sensors/src/smart_home_sensors/sensor_notifier_node.py
@@ -47,7 +47,7 @@ class Notifier(object):
            (self.criteria == 'le' and value.data <= self.notification_value ) or \
            (self.criteria == 'ne' and value.data != self.notification_value ):
             self.pub.publish(Notification(str(uuid.uuid1()), \
-            self.notification_text.replace(_repl, str(value.data)), \
+            self.notification_text.replace(self._repl, str(value.data)), \
             self.notification_level, self.notification_target, "", ()))
 
 if __name__ == "__main__":
